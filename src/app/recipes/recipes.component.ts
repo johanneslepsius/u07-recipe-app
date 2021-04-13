889e16c2e299ab545a888498f08766bcf1addb40
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-04-13 22:43:47 +0200

    add client side filtering

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -9,14 +9,66 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class RecipesComponent implements OnInit {
   recipes: [{}];
+  filteredRecipes;
+  // when false, the recipes-component shows the 8 first results of the api-answer, 
+  // when true (the user is actively searching for recips) all matching results are shown. 
+  // look in html to understand.
+  filtering = false;
 
   constructor(
     private recipeDataService: RecipeDataService,
     private route: ActivatedRoute
     ) { }
-  handleClick(evt) {
-    console.log(evt);
+
+  filterRecipe(recipe, limiters) {
+    // debugger;
+    let trueCounter = 0;
+    // console.log(recipe)
+    for (let key in limiters){
+      if (key === "mealtype") {
+        // console.log(recipe.recipe.hasOwnProperty('mealType'))
+        // debugger;
+          if (recipe.recipe.hasOwnProperty('mealType') && recipe.recipe.mealType.indexOf(limiters[key]) !== -1){
+            // console.log(limiters[key]);
+            // if (recipe.recipe.mealType.indexOf(limiters[key]) !== -1){
+            trueCounter++;
+            // console.log("hello i exist");
+            // }
+          }
+        }
+      if (key !== "mealtype") {
+        // console.log(recipe.recipe.healthLabels)
+        if (recipe.recipe.healthLabels.indexOf(key) !== -1){
+          trueCounter++;
+        }
+      }
+    }
+    // console.log()
+    return trueCounter === Object.keys(limiters).length;
   }
+
+  onSubmit(recipeForm) {
+    let limiters = recipeForm.value;
+    this.filtering = true;
+    
+    for (let key in limiters){
+      // removing empty values from the form data
+      if (!limiters[key]){
+        delete limiters[key];
+      }
+      // setting the checkbox keys value to key name, for example glutenfree: "true" 
+      // becomes glutenfree: "glutenfree"
+      if (limiters[key] === true){
+        limiters[key] = key;
+      }
+    }
+    
+    this.filteredRecipes = this.recipes.filter(recipe => {
+      return this.filterRecipe(recipe, limiters)
+    })
+    console.log(limiters);
+  }
+
   ngOnInit() {
     this.recipeDataService.getRecipes()
       .subscribe(data => {
@@ -28,3 +80,28 @@ export class RecipesComponent implements OnInit {
   }
 
 }
+
+// https://stackoverflow.com/questions/31831651/javascript-filter-array-multiple-conditions
+
+/*const isMealOK = (meal, limiters) => {
+    let manyCorrects = 0;
+    for (let obj of limiters){
+    if (meal[obj.limit] === obj.value) manyCorrects++
+};
+return manyCorrects === limiters.length
+}
+
+const limiters = [{limit: 'type', value: 'a'}]
+
+const filteredMeals2 = arrObjs.filter(item => {
+    return isMealOK(item, limiters)
+})
+
+sätta recipes till filteredmeals
+
+https://angular.io/guide/forms
+
+bättre länk!
+
+https://www.tektutorialshub.com/angular/angular-template-driven-forms/
+*/
