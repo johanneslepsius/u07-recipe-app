ee1c7b8a2d80a4891ad0a2aac0cf045bdb47b329
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-08-16 12:04:02 +0200

    add env example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  recipe_api: 'https://api.edamam.com',
+  app_id: 'your edamam app id',
+  app_key: 'your edamam app key',
+  user_api: 'https://ipa-epicer.herokuapp.com/api',
+};
